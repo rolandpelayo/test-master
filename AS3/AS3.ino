0bb42f847382fa786c641f9a7e7665b4a9a81f3a
popcorn09 <gericko.maandig@hcdc.edu.ph>
2020-08-24 21:58:19 +0800

    Assignment #3 - Gericko Maandig

---
+++ AS3/AS3.ino
@@ -0,0 +1,20 @@
+/* First Arduino Program
+   by Gericko Maandig
+   on August 24
+*/
+
+void setup()
+{
+  for(int j = 13; j>=7; j--){
+  	pinMode(j, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for(int j = 13; j>=7; j--){
+    digitalWrite(j, HIGH);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(j, LOW);
+  }
+}
---
+++ AS3/README.md
@@ -0,0 +1,8 @@
+## Assignment #3 
+### Gericko Maandig
+### August 24
+### Arduino Strobe Lights
+
+### Link to circuit file [https://www.tinkercad.com/things/4aspCH8ab3l-assignment-3-ipt101]
+
+### Link to circuit file [https://www.tinkercad.com/things/1v6NPYeLOQY-assignment-3-ipt101-breadboard-version]
