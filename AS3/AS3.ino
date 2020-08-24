40e80c63453ed700d6b19750a6f30492a775dde7
Reejohndave <reejohndave.dignos@hcdc.edu.ph>
2020-08-25 07:49:14 +0800

    Assignment #3 - Ree Johndave N. Dignos

---
+++ AS3/AS3.ino
@@ -0,0 +1,37 @@
+/*First Arduino Program
+    by <Ree Johndave N. Dingos>
+    on <August 25, 2020>
+*/
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(11, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(10, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(9, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(9, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(8, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(8, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+  
+}
---
+++ AS3/README.md
@@ -0,0 +1,6 @@
+## Assignment #3 
+### <Ree Johndave N. Dignos >
+### <8/25/2020 7:25AM>
+### Arduino Strobe Lights
+
+### Link to circuit file [https://www.tinkercad.com/things/2rC3LgYnVbd-start-simulating/editel?lessonid=EHD2303J3YPUS5Z&projectid=OIYJ88OJ3OPN3EA&collectionid=OIYJ88OJ3OPN3EA&tenant=circuits#/lesson-viewer]
