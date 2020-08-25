7e5506e768862bdda520d4b2dcb09d2e5dfec070
prcepe2000 <preciouscepe15@gmail.com>
2020-08-25 22:17:32 +0800

    Assignment #3 - Precious Enica Cepe

---
+++ AS3/AS3.ino
@@ -0,0 +1,47 @@
+/* ASSIGNMENT #3
+   by PRECIOUS ENICA CEPE
+   on AUGUST 25, 2020
+*/
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(8, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+}
