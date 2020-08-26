6bcb2b21f38970cef924e744efbb844027b01a0a
barsalotek <frankkenneth.barsalote@hcdc.edu.ph>
2020-08-26 16:06:53 +0800

    LAB Activity #1 - Frank Kenneth C. Barsalote

---
+++ LAB1/LAB.ino
@@ -0,0 +1,38 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(10000); // Wait for 10000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(10000); // Wait for 10000 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(10000); // Wait for 10000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(6, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(6, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(10000); // Wait for 10000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+}
---
+++ LAB1/README.md
@@ -0,0 +1,4 @@
+## Lab Activity #1
+### <Frank Kenneth C. Barsalote>
+### <August 26, 2020 - 2:50pm>
+### Arduino Strobe Lights
