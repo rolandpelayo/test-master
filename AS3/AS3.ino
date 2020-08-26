ef11fda96c58a5d30a14b3ea6727f60c79bdcba6
barsalotek <frankkenneth.barsalote@hcdc.edu.ph>
2020-08-26 15:00:48 +0800

    Assignment #3 - Frank Kenneth C. Barsalote

---
+++ AS3/AS3.ino
@@ -0,0 +1,46 @@
+  /* First Arduino Program
+by <Frank Kenneth C. Barsalote>
+on <August 25, 2020>
+*/
+   void setup()
+{
+    pinMode(13,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(7,OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);
+    delay(1000);
+    digitalWrite(12,HIGH);
+    delay(1000);
+    digitalWrite(12,LOW);
+    delay(1000);
+    digitalWrite(11,HIGH);
+    delay(1000);
+    digitalWrite(11,LOW);
+    delay(1000);
+    digitalWrite(10,HIGH);
+    delay(1000);
+    digitalWrite(10,LOW);
+    delay(1000);
+    digitalWrite(9,HIGH);
+    delay(1000);
+    digitalWrite(9,LOW);
+    delay(1000);
+    digitalWrite(8,HIGH);
+    delay(1000);
+    digitalWrite(8,LOW);
+    delay(1000);
+    digitalWrite(7,HIGH);
+    delay(1000);
+    digitalWrite(7,LOW);
+    delay(1000);
+}
