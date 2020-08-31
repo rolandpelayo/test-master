9670f2608fe8cda5c9aeb772f80ce3df40eab546
owenniegas <wencesleemuel.niegas@hcdc.edu.ph>
2020-08-31 13:01:39 +0800

    Assignment #2 - Wences Leemuel Niegas

---
+++ AS2/AS2.ino
@@ -0,0 +1,18 @@
+AS2
+/* First Arduino Program
+   by Wences Leemuel N. Niegas
+   on 08-31-2020
+*/
+
+void setup()
+{
+	pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(13, HIGH);
+	delay(1000);
+	digitalWrite(13, LOW);
+	delay(1000);
+}
