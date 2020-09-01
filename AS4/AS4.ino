8a51c85efab1025694f6ecdac26f77c4d08216b4
faieya01 <blessa.eyas@hcdc.edu.ph>
2020-09-01 23:20:45 +0800

    Assignment #4 <Fairy Blessa Eyas>

---
+++ AS4/AS4.ino
@@ -0,0 +1,18 @@
+int buttonState = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  buttonState = digitalRead(2);
+  if(buttonState == HIGH){
+    digitalWrite(11, HIGH);
+  }else{
+    digitalWrite(11, LOW);
+  }  
+  delay(2000);
+}
---
+++ AS4/README.md
@@ -0,0 +1,5 @@
+## Assignment #4 
+### <FAIRY BLESSA EYAS>
+### <Date:09-01-2020 and Time Submitted: 11:14 PM>
+### Arduino Object Detector
+### Link to circuit file [<https://www.tinkercad.com/things/0HVkuRjfe6B-surprising-fyyran>]
