28dd172eee977b38cec51175f3d1dbda60dc0c46
NollanJay <nollanjay.galicia@hcdc.edu.ph>
2020-09-20 20:06:21 +0800

    Assignment #5 - Nollan Jay D. Galicia

---
+++ AS5/AS5.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+ 
+// Replace with your network credentials
+
+
+const char* ssid = "GlobeAtHome_5E7BF";
+const char* password = "DES081983DEN";
+ 
+ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
+ 
+String page = "";
+int LEDPin = 2;
+void setup(void){
+  //the HTML of the web page
+  page = "<h1 style=""color:blue"">Galicia NodeMCU Web Server</h1>"
+         "<p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a>"
+         "<p>Name:     Nollan Jay D. Galicia</p><p>\n Address:     Brgy. Miranda, Babak District IGaCoS</p><p>Contact #:   09560882294</p><p>Course & Year: BS Information Technology 2nd Year.<p/><p>Achievements:  Consistent Honor Student from Junior High to Senior High</p>";
+  
+  
+  //make the LED pin output and initially turned off
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(LEDPin, LOW);
+   
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password); //begin WiFi connection
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   
+  server.on("/", [](){
+    server.send(200, "text/html", page);
+  });
+  server.on("/LEDOn", [](){
+    server.send(200, "text/html", page);
+    digitalWrite(LEDPin, LOW);
+    delay(1000);
+  });
+  server.on("/LEDOff", [](){
+    server.send(200, "text/html", page);
+    digitalWrite(LEDPin, HIGH);
+    delay(1000); 
+  });
+  server.begin();
+  Serial.println("Web server started!");
+}
+ 
+void loop(void){
+  server.handleClient();
+}
---
+++ AS5/README.md
@@ -0,0 +1,4 @@
+## Assignment #5
+### Nollan Jay D. Galicia
+### September 20,2020 (8:02 P.M)
+### Creating a NodeMCU WebServer
