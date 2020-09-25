d0c94adf7fd7b4f97b850d94d18e0249f48273c8
faieya01 <blessa.eyas@hcdc.edu.ph>
2020-09-25 19:41:00 +0800

    Assignment#6 Updated Web Server

---
+++ AS6/README.md
@@ -0,0 +1,4 @@
+## Assignment #6
+### <FAIRY BLESSA EYAS>
+## Updated NodeMCU Web Server File System/Spiffs
+### <Date:09-25-2020 and Time Submitted: 7:36 PM>
---
+++ AS6/sketch_sep24a/sketch_sep24a.ino
@@ -0,0 +1,81 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+ 
+// Replace with your network credentials
+const char* ssid = "AN5506-04-FA_a2da8";
+const char* password = "T3amB4hay";
+ 
+ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
+ 
+void setup() {
+  //Setup on-board LED
+  pinMode(2, OUTPUT);
+  
+  //For debugging
+  delay(1000);
+  Serial.begin(115200);
+  //Initiate WiFi Connection
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  //Print your WiFi's SSID (might be insecure)
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  //Print your local IP address (needed for browsing the app)
+  Serial.println(WiFi.localIP());
+ 
+  if(!SPIFFS.begin())
+  {   // Serious problem
+    Serial.println("SPIFFS Mount failed");
+  } else {
+    Serial.println("SPIFFS Mount succesfull");
+  }
+ 
+  server.onNotFound([]() { // If the client requests any URI
+    if (!handleFileRead(server.uri())) // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
+ 
+  //start web server
+  server.begin();
+  //Just stating things
+  Serial.println("HTTP server started");
+}
+void loop() {
+  server.handleClient(); //make the ESP32 respond to web clients
+}
+ 
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "Start.html"; // If a folder is requested, send the index file
+  String contentType = getContentType(path); // Get the MIME type
+  if (SPIFFS.exists(path)) { // If the file exists
+    File file = SPIFFS.open(path, "r"); // Open it
+    size_t sent = server.streamFile(file, contentType); // And send it to the client
+    if(path == "/LedOff.html"){
+      digitalWrite(2, HIGH);
+    }else if(path == "/LedOn.html"){
+      digitalWrite(2, LOW);
+    }
+    file.close(); // Then close the file again
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false; // If the file doesn't exist, return false
+}
+ 
+String getContentType(String filename){
+  if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  return "text/plain";
+}
