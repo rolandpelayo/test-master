60118387ecf060ee61e2cebb730bbeaf87cf6e03
Jay-Algabre <jaymark.algabre@hcdc.edu.ph>
2020-09-27 12:37:20 +0800

    Update nodeMCU WebServer

--- AS5/nodeMCU_WebServer/nodeMCU_WebServer.ino
+++ AS5/nodeMCU_WebServer/nodeMCU_WebServer.ino
@@ -1,13 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
- 
+
 // Replace with your network credentials
-const char* ssid = "LOAD SAD PAG MAY TIME";
-const char* password = "lalalala";
- 
+const char* ssid = "DJR";
+const char* password = "@lgabr3store";
+
 ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
- 
+
 String page = "";
 int LEDPin = 2;
 void setup(void){
@@ -16,12 +16,12 @@ void setup(void){
   //make the LED pin output and initially turned off
   pinMode(LEDPin, OUTPUT);
   digitalWrite(LEDPin, LOW);
-   
+
   delay(1000);
   Serial.begin(115200);
   WiFi.begin(ssid, password); //begin WiFi connection
   Serial.println("");
- 
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -32,7 +32,7 @@ void setup(void){
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-   
+
   server.on("/", [](){
     server.send(200, "text/html", page);
   });
@@ -42,17 +42,17 @@ void setup(void){
     digitalWrite(LEDPin, LOW);
     delay(1000);
   });
-  
+
   server.on("/LEDOff", [](){
     server.send(200, "text/html", page);
     digitalWrite(LEDPin, HIGH);
     delay(1000); 
   });
-  
+
   server.begin();
   Serial.println("You can now control the nodeMCU using Web Server!");
 }
- 
+
 void loop(void){
   server.handleClient();
 }
---
+++ AS6/nodeMCU_WebServer2.ino/nodeMCU_WebServer2/README.md
@@ -0,0 +1,4 @@
+## Assignment No.6 
+### Jaymark Algabre
+### September 27, 2020 /  1:00pm
+### Update nodeMCU Web Server
