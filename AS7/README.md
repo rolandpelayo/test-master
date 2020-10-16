1682e0fd7898d7a4736973e642573abb71cbbadd
karlwinston21 <winston.collantes@hcdc.edu.ph>
2020-10-16 23:39:31 +0800

    Assignment #7 - Karl Winston Collantes

---
+++ AS7/README.md
@@ -0,0 +1,4 @@
+## Assignment #7
+### Karl Winston Collantes
+### October 16, 2020 11:30 PM
+### Displaying Light Sensor Data
---
+++ AS7/data/script.js
@@ -0,0 +1,25 @@
+var x = setInterval(function(){loadData("data.txt", updateData);}, 1000);
+function loadData(url, callback){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+if(this.readyState == 4 && this.status == 200){
+callback.apply(xhttp);
+}
+};
+xhttp.open("GET", url, true);
+xhttp.send();
+}
+function updateData(){
+document.getElementById("data").innerHTML = this.responseText;
+if(parseFloat(this.responseText) > 1 && parseFloat(this.responseText) < 475 ){
+    document.getElementById("data2").innerHTML = "Dark";
+}
+if(parseFloat(this.responseText) > 475 && parseFloat(this.responseText) < 950)
+{
+    document.getElementById("data2").innerHTML = "Darker";
+}
+if(parseFloat(this.responseText) > 950)
+{
+    document.getElementById("data2").innerHTML = "Darkest";
+}
+}
