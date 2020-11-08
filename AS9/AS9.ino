a52f54d05fbee4d6453a5148d1654ac75bb8008d
popconr09 <gericko.maandig@hcdc.edu.ph>
2020-11-08 23:11:07 +0800

    Assignment #9 - Gericko Maandig

---
+++ AS9/AS9.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+#define led 2
+ 
+const char* ssid = "TP-LINK";
+const char* password = "09998321149";
+
+DynamicJsonDocument doc(2048);
+
+ 
+void setup () {
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led,HIGH);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println();
+
+  Serial.print("Connecting.");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    digitalWrite(led,LOW);
+    delay(250);
+    digitalWrite(led,HIGH);
+    delay(250);
+  }
+  
+}
+ 
+void loop() {
+
+  digitalWrite(led, LOW);
+  Serial.println();
+  
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    
+    HTTPClient http;
+ 
+    http.begin("http://cat-fact.herokuapp.com/facts/random");
+    
+    http.GET();
+    
+    String payload = http.getString();
+    
+    DeserializationError error = deserializeJson(doc, payload);
+    
+    if(error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      delay(5000);
+      return;
+    }
+
+    //Print the parse value
+    Serial.print("Type: ");
+    Serial.println(doc["type"].as<char*>());
+
+    Serial.print("Fact: ");
+    Serial.println(doc["text"].as<char*>());
+ 
+    http.end();
+ 
+  }
+
+  digitalWrite(led, HIGH);
+  delay(30000);
+  
+}
---
+++ AS9/README.md
@@ -0,0 +1,6 @@
+## Assignment #9
+### Gericko S. Maandig
+### November 8 2020
+### Web APIs
+### Web API used: http://cat-fact.herokuapp.com
+### GET API Request JSON File: http://cat-fact.herokuapp.com/facts/random
